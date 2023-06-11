292060147d9a695d90abd102a3c5c034b99a5dc3
WALTERB <101801884+WAZZEVER@users.noreply.github.com>
2023-06-11 21:56:48 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,171 +1,22 @@
-const outButton = document.getElementById('logout');
-const inButton = document.getElementById('login');
-const userName = document.getElementById('username');
-const claimcoin = document.getElementById('claimcoin');
+const detect = document.querySelector("#detect");
+const wrapper = document.querySelector(".wrapper");
+const overlay = document.createElement("div"); // Create overlay element
+overlay.classList.add("overlay"); // Add overlay class to the element
+document.body.insertBefore(overlay, detect); // Insert overlay before the detect element
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDbCzXFmL3Hyex2uzwiYRaWNvPCWJLudDU",
-  authDomain: "testwazz-460a6.firebaseapp.com",
-  projectId: "testwazz-460a6",
-  storageBucket: "testwazz-460a6.appspot.com",
-  messagingSenderId: "226425030359",
-  appId: "1:226425030359:web:718330be5829d30fab9292",
-  measurementId: "G-TWQL3V0EVE"
-};
+const button = wrapper.querySelector("button");
 
-var userEmail; // Declare the userEmail variable
+let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
+for (let item of adClasses) {
+  detect.classList.add(item);
+}
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-// Set up Google provider for Firebase Authentication
-const provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is logged in
-    userEmail = user.email; // Assign the email value to userEmail
+let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+if (!wrapper.classList.contains("show")) {
+  overlay.style.display = "none"; // Hide the overlay if no adblock is detected
+  getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+}
 
-    var userRef = db.collection('faucet').doc(userEmail);
-    userRef.get().then(function(doc) {
-      if (doc.exists) {
-        const tokenbal = doc.data().balance;
-        const coinbal = doc.data().token;
-        document.getElementById("wazztoken").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"/></svg> Wazz Token: ${tokenbal}`
-        document.getElementById("coin").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"/></svg> Coin: ${coinbal}`
-
-      } else {
-
-        document.getElementById("wazztoken").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"/></svg> Wazz Token: 0`
-        document.getElementById("coin").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 80c0 18-14.3 34.6-38.4 48c-29.1 16.1-72.5 27.5-122.3 30.9c-3.7-1.8-7.4-3.5-11.3-5C300.6 137.4 248.2 128 192 128c-8.3 0-16.4 .2-24.5 .6l-1.1-.6C142.3 114.6 128 98 128 80c0-44.2 86-80 192-80S512 35.8 512 80zM160.7 161.1c10.2-.7 20.7-1.1 31.3-1.1c62.2 0 117.4 12.3 152.5 31.4C369.3 204.9 384 221.7 384 240c0 4-.7 7.9-2.1 11.7c-4.6 13.2-17 25.3-35 35.5c0 0 0 0 0 0c-.1 .1-.3 .1-.4 .2l0 0 0 0c-.3 .2-.6 .3-.9 .5c-35 19.4-90.8 32-153.6 32c-59.6 0-112.9-11.3-148.2-29.1c-1.9-.9-3.7-1.9-5.5-2.9C14.3 274.6 0 258 0 240c0-34.8 53.4-64.5 128-75.4c10.5-1.5 21.4-2.7 32.7-3.5zM416 240c0-21.9-10.6-39.9-24.1-53.4c28.3-4.4 54.2-11.4 76.2-20.5c16.3-6.8 31.5-15.2 43.9-25.5V176c0 19.3-16.5 37.1-43.8 50.9c-14.6 7.4-32.4 13.7-52.4 18.5c.1-1.8 .2-3.5 .2-5.3zm-32 96c0 18-14.3 34.6-38.4 48c-1.8 1-3.6 1.9-5.5 2.9C304.9 404.7 251.6 416 192 416c-62.8 0-118.6-12.6-153.6-32C14.3 370.6 0 354 0 336V300.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 342.6 135.8 352 192 352s108.6-9.4 148.1-25.9c7.8-3.2 15.3-6.9 22.4-10.9c6.1-3.4 11.8-7.2 17.2-11.2c1.5-1.1 2.9-2.3 4.3-3.4V304v5.7V336zm32 0V304 278.1c19-4.2 36.5-9.5 52.1-16c16.3-6.8 31.5-15.2 43.9-25.5V272c0 10.5-5 21-14.9 30.9c-16.3 16.3-45 29.7-81.3 38.4c.1-1.7 .2-3.5 .2-5.3zM192 448c56.2 0 108.6-9.4 148.1-25.9c16.3-6.8 31.5-15.2 43.9-25.5V432c0 44.2-86 80-192 80S0 476.2 0 432V396.6c12.5 10.3 27.6 18.7 43.9 25.5C83.4 438.6 135.8 448 192 448z"/></svg> Coin: ${coinbal}`
-
-      }
-    })
-    document.getElementById("logout").style.display = "block";
-    document.getElementById("login").style.display = "none";
-    userName.textContent = `${userEmail}`;
-    document.getElementById("pic").src = user.photoURL;
-  } else {
-    document.getElementById("login").style.display = "block";
-    document.getElementById("logout").style.display = "none";
-    userName.textContent = 'Not logged in!';
-    document.getElementById("pic").src = "https://th.bing.com/th/id/R.9f0b428f466adad9b39f553a004efb2e?rik=syrpeNnxLeFYjg&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_83486.png&ehk=9xpQAl0bzAu0Dih5xrvddlyRzSB05za79%2f64VtPMslQ%3d&risl=&pid=ImgRaw&r=0";
-  }
-});
-
-
-// Logout button click event
-outButton.addEventListener('click', function() {
-  Swal.fire({
-    title: 'Logout Confirmation',
-    text: 'Are you sure you want to logout?',
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonText: 'Yes, logout',
-    cancelButtonText: 'Cancel'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      firebase.auth().signOut().then(() => {
-        console.log("User signed out successfully");
-        Swal.fire({
-          icon: 'success',
-          title: 'Logged Out',
-          text: 'You have been logged out successfully!'
-        });
-      }).catch((error) => {
-        console.log(error.message);
-      });
-    }
-  });
-});
-
-
-// Login button click event
-inButton.addEventListener('click', function() {
-  firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-      console.log("User signed in successfully");
-      Swal.fire({
-        icon: 'success',
-        title: 'Logged In',
-        text: 'You have been logged in successfully!'
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      Swal.fire({
-        icon: 'error',
-        title: 'Login Error',
-        text: 'An error occurred during the login process. Please try again.'
-      });
-    });
-});
-
-// Get the profile dropdown element
-const profileDropdown = document.getElementById('profileDropdown');
-// Get the profile link element
-const profileLink = document.getElementById('profileLink');
-
-// Add a click event listener to the profile link
-profileLink.addEventListener('click', function() {
-  // Toggle the is-active class on the profile dropdown
-  profileDropdown.classList.toggle('is-active');
-});
-
-document.getElementById("redirectLink").addEventListener("click", function() {
-  var userRef = db.collection('faucet').doc(userEmail);
-  userRef.get().then(function(doc) {
-    if (doc.exists) {
-      var cooldown = new Date(doc.data().shrinkcool).getTime();
-      var currentTime = new Date().getTime();
-      var timeDifference = currentTime - cooldown;
-      var cooldownHours = timeDifference / (1000 * 60 * 60); // Convert milliseconds to hours
-
-      if (cooldownHours >= 24) {
-        window.location.href = "https://shrinke.me/faucetweb";
-      } else {
-        var remainingCooldownHours = 24 - cooldownHours;
-        alert("Please wait for " + remainingCooldownHours + " more hours doing the shortlink again!");
-      }
-
-    } else {
-             alert("Pls login first! You wont get any token if you havent login!");
-      window.location.href = "https://shrinke.me/faucetweb";
-    }
-  })
-});
-
-var claimButton = document.getElementById('claimcoin');
-
-// Add a click event listener to the button
-claimButton.addEventListener('click', function() {
-  if (userEmail === null) {
-    alert("Please login first!");
-  } else {
-    // Add 1 token to the user's balance in the database
-    var db = firebase.firestore();
-    var userRef = db.collection('faucet').doc(userEmail);
-    userRef.get().then(function(doc) {
-      if (doc.exists) {
-        var currentBalance = doc.data().balance || 0;
-        if (currentBalance < 1) {
-          alert("Not enough coin!");
-        } else {
-          var newBalance = currentBalance - 1;
-          userRef.update({
-            balance: newBalance
-          })
-            .then(function() {
-              console.log('Token added successfully');
-                         alert("Error!");
-            })
-            .catch(function(error) {
-              console.log('Error updating balance:', error);
-                         alert("Error!");
-            });
-        }
-      } else {
-        alert("Pls login first!");
-      }
-    });
-  }
+button.addEventListener("click", () => {
+  window.location.reload();
 });
